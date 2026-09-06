9ea494d08dc852d3956a127a6d19927931fb8131
GitTanish <tanishsaroj6781@gmail.com>
2026-09-07 02:25:42 +0530

    Pydantic types

--- Pydantic/1_pydantic_why.py
+++ Pydantic/1_pydantic_why.py
@@ -1,34 +1,37 @@
-from pydantic import BaseModel
-from typing import List, Dict
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List, Dict, Optional
 
 class Patient(BaseModel):
     name: str
+    email: EmailStr
+    Url: AnyUrl
     age: int
     weight: float
-    married: bool
-    allergies: List[str]
+    married: Optional[bool] =False
+    allergies: Optional[List[str]] =None
     contact_details: Dict[str, str]
 
 def insert(patient: Patient): # patient:Patient is annotation not enforcement
     print(patient.name)
+    print(patient.email)
+    print(patient.Url)
     print(patient.age)
     print(patient.allergies)
+    print(patient.married)
     print('Inserted')
 
 # patient_info= {'name':'doof', 'age':42}
 # patient1 = Patient(**patient_info)
 patient1 = Patient(
     name='doof',
+    email='panchax@killi.com',
+    Url = 'https://linkedin.com/13',
     age=42,
     weight=62.3,
-    married=True,
     allergies=['dust', 'peanut'],
     contact_details={
-        'email': 'xyz@abc.com',
         'phone': '23940232'
     }
 )
-
-insert(patient1)
 
 insert(patient1)
