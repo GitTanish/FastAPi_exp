9ded50167e9a9f0715231cb5395b083b7dc7e0b0
GitTanish <tanishsaroj6781@gmail.com>
2026-09-06 17:09:02 +0530

    Type annotation

---
+++ Pydantic/1_pydantic_why.py
@@ -0,0 +1,9 @@
+def insert(name:str,age:int):
+# type annotation is still not type enforcement
+
+
+    print(name)
+    print(age)
+    print('inserted into database')
+
+insert('Doof','forty two')
