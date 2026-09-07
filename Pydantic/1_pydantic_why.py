1fafa4e4cd0a88cbf856f987297b8fcadea3fc90
GitTanish <tanishsaroj6781@gmail.com>
2026-09-08 01:18:21 +0530

    Field, Annotated, Desc,

--- Pydantic/1_pydantic_why.py
+++ Pydantic/1_pydantic_why.py
@@ -1,14 +1,14 @@
 from pydantic import BaseModel, EmailStr, AnyUrl, Field
 from typing import List, Dict, Optional, Annotated
-# field can be used for data validation and to attach the metadata with Annotated
+# field can be used for data validation and to attach metadata with Annotated
 class Patient(BaseModel):
     name: Annotated[str, Field(max_length=50, title='Name of the patient',description='Name should be less than 50 characters', examples=['Doof','Bidoof'])]
     email: EmailStr
     Url: AnyUrl
     age: int= Field(gt=0, lt=120)
     weight: Annotated[float, Field(gt=0, strict=True)]
-    married: Optional[bool] =False
-    allergies: Optional[List[str]] = Field(max_length=5)
+    married: Annotated[bool, Field(default=False, description="Is the patient married or not?")]
+    allergies: Annotated[Optional[List[str]], Field(default=None, max_length=5)]
     contact_details: Dict[str, str]
 
 def insert(patient: Patient): # patient:Patient is annotation not enforcement
@@ -28,6 +28,7 @@ patient1 = Patient(
     Url = 'https://linkedin.com/13',
     age=42,
     weight=62.3,
+    married=True,
     allergies=['dust', 'peanut'],
     contact_details={
         'phone': '23940232'
